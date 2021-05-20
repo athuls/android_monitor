09e1d43a2d70850f2f8931bce3ebf4aeb14df162
vszabo2 <54809586+vszabo2@users.noreply.github.com>
2021-05-19 23:23:00 -0500

    Add UnsaturatedLayerByLayerApp

---
+++ UnsaturatedLayerByLayerApp/app/src/main/java/com/example/tfbenchmark/Inferer.java
@@ -0,0 +1,115 @@
+/*
+Based on https://github.com/tensorflow/examples/blob/master/lite/examples/object_detection/android/lib_interpreter/src/main/java/org/tensorflow/lite/examples/detection/tflite/TFLiteObjectDetectionAPIModel.java
+Adapted by Victor Szabo
+
+Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+package com.example.tfbenchmark;
+
+import android.os.Trace;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Inferer {
+    private static final float IMAGE_MEAN = 127.5f;
+    private static final float IMAGE_STD = 127.5f;
+
+    private int inputSize;
+    private int batchSize;
+    private ByteBuffer imgData;
+    private ByteBuffer outputData;
+
+    private Object[] inputArray;
+    private Map<Integer, Object> outputMap;
+
+    private Interpreter tfLite;
+
+    Inferer(String modelPath, int inputSize, int batchSize) {
+        this.inputSize = inputSize;
+        this.batchSize = batchSize;
+
+        try {
+            loadModel(modelPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        int numBytesPerChannel = 4;
+        imgData = ByteBuffer.allocateDirect(batchSize * inputSize * inputSize * 3 * numBytesPerChannel);
+        imgData.order(ByteOrder.nativeOrder());
+        inputArray = new Object[]{imgData};
+
+        outputData = ByteBuffer.allocateDirect(tfLite.getOutputTensor(0).numBytes());
+        outputData.order(ByteOrder.nativeOrder());
+        outputMap = new HashMap<>();
+        outputMap.put(0, outputData);
+    }
+
+    /**
+     * Memory-map the model file in Assets.
+     */
+    private void loadModel(String modelPath)
+            throws IOException {
+        FileInputStream inputStream = new FileInputStream(modelPath);
+        FileChannel fileChannel = inputStream.getChannel();
+        MappedByteBuffer modelFile = fileChannel.map(FileChannel.MapMode.READ_ONLY, fileChannel.position(), fileChannel.size());
+        Interpreter.Options options = new Interpreter.Options();
+        options.setNumThreads(2);
+        options.setUseXNNPACK(true);
+        //options.addDelegate(new GpuDelegate());
+        tfLite = new Interpreter(modelFile, options);
+        tfLite.resizeInput(0, new int[]{batchSize, inputSize, inputSize, 3});
+        tfLite.allocateTensors();
+    }
+
+    public void loadImage(int[][] batch) {
+        if (batch.length != batchSize) {
+            throw new AssertionError("Incorrect batch size");
+        }
+        for (int[] intValues : batch) {
+            if (intValues.length != inputSize * inputSize) {
+                throw new AssertionError("Incorrect input size");
+            }
+            for (int i = 0; i < inputSize * inputSize; ++i) {
+                int pixelValue = intValues[i];
+                imgData.putFloat((((pixelValue >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                imgData.putFloat((((pixelValue >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                imgData.putFloat(((pixelValue & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+            }
+        }
+    }
+
+    public long infer() {
+        outputData.rewind();
+
+        long startTime = System.nanoTime();
+        //Trace.beginSection("runInference");
+        tfLite.runForMultipleInputsOutputs(inputArray, outputMap);
+        //Trace.endSection();
+        long endTime = System.nanoTime();
+
+        return endTime - startTime;
+    }
+}
---
+++ UnsaturatedLayerByLayerApp/app/src/main/java/com/example/tfbenchmark/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.tfbenchmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.media.ThumbnailUtils;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int IMAGE_SIZE = 224;
+    private static final int BATCH_SIZE = 1;
+    private static final int FRAME_RATE = 4;
+    private static final long SECONDS_TO_RUN = 60 * 60;
+
+    private TextView currentEpochView;
+    private EditText modelFileView;
+
+    private int[][] images;
+    private int currentEpoch = 0;
+    private int currentImage = 0;
+    private Inferer inferer;
+    private int[][] batch = new int[BATCH_SIZE][];
+
+    private long startTime = -1;
+    private long totalInferenceTime = 0;
+
+    private final Runnable updateDisplay = new Runnable() {
+        @Override
+        public void run() {
+            int numImagesSoFar = currentEpoch * images.length + currentImage;
+            long avgTime = totalInferenceTime / numImagesSoFar;
+            String formatString = "Current Epoch: %d\nAverage Time per Image: %d ns\n" +
+                    "Total time elapsed: %d ns\nTotal images processed: %d";
+            currentEpochView.setText(String.format(formatString, currentEpoch, avgTime,
+                    System.nanoTime() - startTime, numImagesSoFar));
+        }
+    };
+
+    private final TimerTask createInferer = new TimerTask() {
+        @Override
+        public void run() {
+            inferer = new Inferer(modelFileView.getText().toString(), IMAGE_SIZE, BATCH_SIZE);
+            System.arraycopy(images, currentImage, batch, 0, BATCH_SIZE);
+            inferer.loadImage(batch);
+        }
+    };
+
+    private final TimerTask runNextInference = new TimerTask() {
+        @Override
+        public void run() {
+            if (startTime == -1) {
+                startTime = System.nanoTime();
+            }
+
+            totalInferenceTime += inferer.infer();
+            currentImage += BATCH_SIZE;
+            if (currentImage >= images.length) {
+                currentImage = 0;
+                currentEpoch++;
+                runOnUiThread(updateDisplay);
+            }
+
+            if (System.nanoTime() - startTime > SECONDS_TO_RUN * 1_000_000_000) {
+                cancel(); // cancel the TimerTask
+                runOnUiThread(updateDisplay);
+                new ToneGenerator(AudioManager.STREAM_MUSIC, ToneGenerator.MAX_VOLUME).startTone(ToneGenerator.TONE_DTMF_0, 500);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        currentEpochView = findViewById(R.id.currentEpoch);
+        modelFileView = findViewById(R.id.modelFileInput);
+        Button runButton = findViewById(R.id.runButton);
+        runButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // run inference
+                Timer timer = new Timer("ScheduledInference");
+                timer.schedule(createInferer, 0);
+                timer.scheduleAtFixedRate(runNextInference, 1000, 1000 / FRAME_RATE);
+                currentEpochView.setText("Started");
+            }
+        });
+
+        // load images
+        File dir = new File("/sdcard/TfBenchmark/images");
+        File[] imageNames = dir.listFiles();
+        int numImages = imageNames.length;
+        images = new int[numImages][];
+        for (int i = 0; i < numImages; i++) {
+            Bitmap b = BitmapFactory.decodeFile(imageNames[i].getPath());
+            b = ThumbnailUtils.extractThumbnail(b, IMAGE_SIZE, IMAGE_SIZE, ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+
+            images[i] = new int[IMAGE_SIZE * IMAGE_SIZE];
+            b.getPixels(images[i], 0, b.getWidth(), 0, 0, b.getWidth(), b.getHeight());
+        }
+
+        currentEpochView.setText("Images loaded");
+    }
+}
